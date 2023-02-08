ccd5a81f6fbf9e0b25fedf66245d488f33d98fb0
jesperkeden <83838381+jesperkeden@users.noreply.github.com>
2023-02-08 12:38:50 +0100

    Revert "Merge branch 'master' of https://github.com/jesperkeden/WeatherData"
    This reverts commit 1356879278a1dbd01f20a30fc498dd0381bdfa46, reversing
    changes made to 9787ad77a3dd864e5458950a9e8624c01dc01804.

--- Menu.cs
+++ Menu.cs
@@ -4,42 +4,42 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WeatherData.Models;
 
-namespace WeatherData.Classes
+namespace WeatherData
 {
     internal class Menu
     {
         private int SelectedIndex;
         private string[] Options;
+
         public Menu(string[] options)
         {
             Options = options;
             SelectedIndex = 0;
         }
 
-        private static void RunMainMenu(List<iMeasurable> classList)
+        private static void RunMainMenu()
         {
-            var mainMenuEnums = Enum.GetNames(typeof(Enums.MainMenu));
+            var mainMenuEnums = (Enum.GetNames(typeof(Enums.MainMenu)));
             Menu mainMenu = new Menu(mainMenuEnums);
             int selectedIndex = mainMenu.Run();
 
             switch (selectedIndex)
             {
                 case 0:
-                    RunInsideMenu(classList[0]);
+                    RunInsideMenu();
                     break;
                 case 1:
-                    RunOutsideMenu(classList[1]);
+                    RunOutsideMenu();
                     break;
                 case 2:
                     Exit();
                     break;
             }
         }
-        public static void RunInsideMenu(iMeasurable insideClass)
+        public static void RunInsideMenu()
         {
-            var InsideMenuEnums = Enum.GetNames(typeof(Enums.InsideMenu));
+            var InsideMenuEnums = (Enum.GetNames(typeof(Enums.InsideMenu)));
             Menu insideMenu = new Menu(InsideMenuEnums);
             int selectedIndex = insideMenu.Run();
 
@@ -47,34 +47,25 @@ namespace WeatherData.Classes
             {
                 case 0:
                     Console.WriteLine("Medeltemperatur för valt datum (sökmöjlighet med validering)");
-                    Console.Clear();
-                    insideClass.AvgValues();
-                    Console.ReadKey();
                     break;
                 case 1:
                     //HotNCold();
                     Console.WriteLine("Sortering av varmast till kallaste dagen enligt medeltemperatur per dag");
-                    Console.Clear();
-                    insideClass.MaxMinWeatherDay("Temperature");
-                    Console.ReadKey();
                     break;
                 case 2:
                     Console.WriteLine("Sortering av torrast till fuktigaste dagen enligt medeltemperatur per dag");
-                    Console.Clear();
-                    insideClass.MaxMinWeatherDay("Humidity");
-                    Console.ReadKey();
                     break;
                 case 3:
                     Console.WriteLine("Sortering av minst till störst risk av mögel");
                     break;
                 case 4:
-                    // Back to RunMainMenu
+                    RunMainMenu();
                     break;
             }
         }
-        public static void RunOutsideMenu(iMeasurable outsideClass)
+        public static void RunOutsideMenu()
         {
-            var outsideMenuEnums = Enum.GetNames(typeof(Enums.OutsideMenu));
+            var outsideMenuEnums = (Enum.GetNames(typeof(Enums.OutsideMenu)));
             Menu outsideMenu = new Menu(outsideMenuEnums);
             int selectedIndex = outsideMenu.Run();
 
@@ -82,21 +73,12 @@ namespace WeatherData.Classes
             {
                 case 0:
                     Console.WriteLine("Medeltemperatur för valt datum (sökmöjlighet med validering)");
-                    Console.Clear();
-                    outsideClass.AvgValues();
-                    Console.ReadKey();
                     break;
                 case 1:
                     Console.WriteLine("Sortering av varmast till kallaste dagen enligt medeltemperatur per dag");
-                    Console.Clear();
-                    outsideClass.MaxMinWeatherDay("Temperature");
-                    Console.ReadKey();
                     break;
                 case 2:
                     Console.WriteLine("Sortering av torrast till fuktigaste dagen enligt medeltemperatur per dag");
-                    Console.Clear();
-                    outsideClass.MaxMinWeatherDay("Humidity");
-                    Console.ReadKey();
                     break;
                 case 3:
                     Console.WriteLine("Sortering av minst till störst risk av mögel");
@@ -108,13 +90,13 @@ namespace WeatherData.Classes
                     Console.WriteLine("Datum för meteorologisk vinter(OBS Mild vinter!)");
                     break;
                 case 6:
-                    // Back to RunMainMenu
+                    RunMainMenu();
                     break;
             }
         }
-        public static void Start(List<iMeasurable> classList)
+        public static void Start()
         {
-            RunMainMenu(classList);
+            RunMainMenu();
         }
         private void DisplayOptions()
         {
