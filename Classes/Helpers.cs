eb0f74383cbb4f3a59304e9f5762c8affb862b59
jesperkeden <83838381+jesperkeden@users.noreply.github.com>
2023-02-10 07:57:33 +0100

    liten update

--- Classes/Helpers.cs
+++ Classes/Helpers.cs
@@ -12,6 +12,7 @@ namespace WeatherData.Classes
     internal class Helpers
     {
         private const string filePath = "../../../Data/tempdata5-med fel.txt";
+        private const string outPath = "../../../Data/test.txt";
 
         static bool IsValidDate(string date, out DateTime inputDate)
         {
@@ -51,105 +52,209 @@ namespace WeatherData.Classes
                 input = Console.ReadLine();
             }
         }
-        public static List<string> CreateListOfData()
-        {
-            List<string> lines = new List<string>();
-            using (StreamReader reader = new StreamReader(filePath))
-            {
-                while (!reader.EndOfStream)
-                {
-                    lines.Add(reader.ReadLine());
-                }
-            }
-            return lines;
-        }
-
         public static List<FileData> ReadTextFile(string filePath)
         {
             List<FileData> logs = new List<FileData>();
 
-            using (StreamReader reader = new StreamReader(filePath))
+            if (!File.Exists(filePath))
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    Match match = Regex.Match(line, @"(\d{4})([-.])(0[1-9]|1[0-2])([-.])(0[1-9]|[1-2][0-9]|3[0-1])([\w\s])(0[0-9]|1[0-9]|2[0-3])([:;.,-_])([0-5][0-9])([:;.,-_])([0-5][0-9]),([Inne|Ute]+),(-?\d+[\W\S]?\d*?),(-?\d+[\W\S]?\d+)?$");
+                throw new FileNotFoundException("The file could not be found at the specified path: " + filePath);
+            }
 
-                    if (match.Success)
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
                     {
-                        try
+                        Match match = Regex.Match(line, @"(\d{4})([-.])(0[1-9]|1[0-2])([-.])(0[1-9]|[1-2][0-9]|3[0-1])([\w\s])(0[0-9]|1[0-9]|2[0-3])([:;.,-_])([0-5][0-9])([:;.,-_])([0-5][0-9]),([Inne|Ute]+),(-?\d+[\W\S]?\d*?),(-?\d+[\W\S]?\d+)?$");
+
+                        if (match.Success)
                         {
-                            DateTime date = DateTime.ParseExact(
-                                match.Groups[1].Value + "-" + match.Groups[3].Value + "-" + match.Groups[5].Value + " " +
-                                match.Groups[7].Value + ":" + match.Groups[9].Value + ":" + match.Groups[11].Value,
-                                "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
-                            string location = match.Groups[12].Value;
-                            double temperature;
-                            if (!double.TryParse(match.Groups[13].Value, NumberStyles.AllowLeadingSign | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out temperature))
+                            try
                             {
-                                Console.WriteLine("Could not parse temperature value.");
-                                continue;
+                                DateTime date = DateTime.ParseExact(
+                                    match.Groups[1].Value + "-" + match.Groups[3].Value + "-" + match.Groups[5].Value + " " +
+                                    match.Groups[7].Value + ":" + match.Groups[9].Value + ":" + match.Groups[11].Value,
+                                    "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+
+                                if (date.Month == 5 && date.Year == 2016 || date.Month == 1 && date.Year == 2017)
+                                {
+                                    continue;
+                                }
+                                string location = match.Groups[12].Value;
+                                double temperature;
+                                if (!double.TryParse(match.Groups[13].Value, NumberStyles.AllowLeadingSign | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out temperature))
+                                {
+                                    Console.WriteLine("Could not parse temperature value.");
+                                    continue;
+                                }
+                                double humidity;
+                                if (!double.TryParse(match.Groups[14].Value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out humidity))
+                                {
+                                    Console.WriteLine("Could not parse humidity value.");
+                                    continue;
+                                }
+                                FileData fileData = new FileData(location, temperature, humidity, date);
+                                logs.Add(fileData);
                             }
-                            double humidity;
-                            if (!double.TryParse(match.Groups[14].Value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out humidity))
+                            catch (Exception ex)
                             {
-                                Console.WriteLine("Could not parse humidity value.");
-                                continue;
+                                Console.WriteLine("Error parsing line: " + ex.Message);
                             }
-                            FileData fileData = new FileData(location, temperature, humidity, date);
-                            logs.Add(fileData);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine("Error parsing line: " + ex.Message);
                         }
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading file: " + ex.Message);
+                throw;
+            }
             return logs;
         }
-
-
-
-        //SORTING METHODS     No references so far..
-        public static void SortByMonth()
+        public static void PrintToFile()
         {
-            List<string> list = CreateListOfData();
-            string targetMonth = "05";
 
-            var filteredData = from line in list
-                               let parts = line.Split(',')
-                               let dateTime = parts[0]
-                               let dateTimeParts = dateTime.Split(' ')
-                               let date = dateTimeParts[0]
-                               let month = date.Split('-')[1]
-                               where month == targetMonth
-                               select line;
+            List<FileData> logs = Helpers.ReadTextFile(filePath);
+            //Temperature
+            Dictionary<string, double> averageTemperatureInne = new Dictionary<string, double>();
+            Dictionary<string, double> averageTemperatureUte = new Dictionary<string, double>();
+            Dictionary<string, double> countTemperatureInne = new Dictionary<string, double>();
+            Dictionary<string, double> countTemperatureUte = new Dictionary<string, double>();
 
-            foreach (string line in filteredData)
+            //Humidity
+            Dictionary<string, double> averageHumidityInne = new Dictionary<string, double>();
+            Dictionary<string, double> averageHumidityUte = new Dictionary<string, double>();
+            Dictionary<string, double> countHumidityInne = new Dictionary<string, double>();
+            Dictionary<string, double> countHumidityUte = new Dictionary<string, double>();
+
+            //Mold
+            Dictionary<string, double> averageRiskInne = new Dictionary<string, double>();
+            Dictionary<string, double> averageRiskUte = new Dictionary<string, double>();
+            Dictionary<string, double> countRiskInne = new Dictionary<string, double>();
+            Dictionary<string, double> countRiskUte = new Dictionary<string, double>();
+
+
+
+            foreach (FileData log in logs)
             {
-                Console.WriteLine(line);
-            }
-        }
-   
-
-        // Secure number input
-        internal static int TryNumber(int number, int minValue, int maxValue)
-        {
-            bool correctInput = false;
-
-            while (!correctInput)
-            {
-                if (!int.TryParse(Console.ReadLine(), out number) || number > maxValue || number < minValue)
+                string month = log.DateTime.ToString("MMM");
+                if (log.Location == "Inne")
                 {
-                    Console.Write("Fel inmatning. Försök igen: ");
+                    if (!averageTemperatureInne.ContainsKey(month))
+                    {
+                        averageTemperatureInne[month] = 0;
+                        averageHumidityInne[month] = 0;
+                        averageRiskInne[month] = 0;
+
+                        countTemperatureInne[month] = 0;
+                        countHumidityInne[month] = 0;
+                        countRiskInne[month] = 0;
+                    }
+                    averageTemperatureInne[month] += log.Temperature;
+                    averageHumidityInne[month] += log.Humidity;
+                    averageRiskInne[month] += log.RiskPercentage;
+
+                    countTemperatureInne[month]++;
+                    countHumidityInne[month]++;
+                    countRiskInne[month]++;
                 }
                 else
                 {
-                    correctInput = true;
+                    if (!averageTemperatureUte.ContainsKey(month))
+                    {
+                        averageTemperatureUte[month] = 0;
+                        averageHumidityUte[month] = 0;
+                        averageRiskUte[month] = 0;
+
+                        countTemperatureUte[month] = 0;
+                        countHumidityUte[month] = 0;
+                        countRiskUte[month] = 0;
+                    }
+                    averageTemperatureUte[month] += log.Temperature;
+                    averageHumidityUte[month] += log.Humidity;
+                    averageRiskUte[month] += log.RiskPercentage;
+
+                    countTemperatureUte[month]++;
+                    countHumidityUte[month]++;
+                    countRiskUte[month]++;
                 }
             }
-            return number;
+
+            foreach (var entry in averageTemperatureInne)
+            {
+                string month = entry.Key;
+                averageTemperatureInne[month] /= countTemperatureInne[month];
+                averageHumidityInne[month] /= countHumidityInne[month];
+                averageRiskInne[month] /= countRiskInne[month];
+            }
+
+            foreach (var entry in averageTemperatureUte)
+            {
+                string month = entry.Key;
+                averageTemperatureUte[month] /= countTemperatureUte[month];
+                averageHumidityUte[month] /= countHumidityUte[month];
+                averageRiskUte[month] /= countRiskUte[month];
+            }
+
+
+            using (StreamWriter writer = new StreamWriter(outPath))
+            {
+                writer.WriteLine("---------------");
+                writer.WriteLine("Avg Temperature");
+                writer.WriteLine("---------------");
+                foreach (var entry in averageTemperatureInne)
+                {
+                    string month = entry.Key;
+                    double avgInneTemp = entry.Value;
+                    double avgUteTemp = averageTemperatureUte[month];
+
+                    writer.WriteLine(month + "\t" +
+                        "Inside: " + avgInneTemp.ToString("0.00", CultureInfo.InvariantCulture) + "\t" +
+                        "Outside: " + avgUteTemp.ToString("0.00", CultureInfo.InvariantCulture));
+                }
+
+                writer.WriteLine("");
+                writer.WriteLine("------------");
+                writer.WriteLine("Avg Humidity");
+                writer.WriteLine("------------");
+                foreach (var entry in averageHumidityInne)
+                {
+                    string month = entry.Key;
+                    double avgInneHum = averageHumidityInne[month];
+                    double avgUteHum = averageHumidityUte[month];
+
+                    writer.WriteLine(month + "\t" +
+                        "Inside: " + avgInneHum.ToString("0.00", CultureInfo.InvariantCulture) + "\t" +
+                        "Outside: " + avgUteHum.ToString("0.00", CultureInfo.InvariantCulture));
+                }
+
+                writer.WriteLine("");
+                writer.WriteLine("---------------------------");
+                writer.WriteLine("Avg Risk for mold per month");
+                writer.WriteLine("---------------------------");
+                foreach (var entry in averageRiskInne)
+                {
+                    string month = entry.Key;
+                    double avgInneRisk = averageRiskInne[month];
+                    double avgUteRisk = averageRiskUte[month];
+
+                    writer.WriteLine(month + "\t" +
+                        "Inside: " + avgInneRisk.ToString("0.00", CultureInfo.InvariantCulture) + "\t" +
+                        "Outside: " + avgUteRisk.ToString("0.00", CultureInfo.InvariantCulture));
+                }
+
+                //HÄR ÄR JAG!!!!
+                writer.WriteLine("");
+                writer.WriteLine("------------------");
+                writer.WriteLine("Meterological Fall");
+                writer.WriteLine("------------------");
+                writer.WriteLine();
+
+            }
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,12 +6,12 @@ using WeatherData.Data;
 using WeatherData.Models;
 
 
+
 namespace WeatherData
 {
     //delegate void LogToFileDelegate(string message);
     internal class Program
     {
-        private const string filePath = "../../../Data/tempdata5-med fel.txt";
         static void Main(string[] args)
         {
             //Console.CursorVisible = false;
@@ -19,80 +19,15 @@ namespace WeatherData
 
             //while (runProgram)
             //{
-            //    List<iMeasurable> classList = Data1.CreateData();
+            //    List<iMeasurable> classList = Models.Data.CreateData();
 
             //    Menu.Start(classList);
 
             //}
-            //Data1.Test();
-            //Dictionary<DateTime, double> testdic = new();
-            //testdic = MoldRiskCalculator.CalculateAverageMoldRiskPerMonth();
-            //foreach (KeyValuePair<DateTime, double> kvp in testdic)
-            //{
-            //    Console.WriteLine(kvp.Key.ToString("MMMM yyyy") + " Value: " + kvp.Value);
-            //}
 
-            Dictionary<DateTime, double> testdic2 = new();
-            testdic2 = MoldRiskCalculator.CalculateAverageMoldRiskPerday();
-            foreach (KeyValuePair<DateTime, double> kvp in testdic2)
-            {
-                Console.WriteLine(kvp.Key.ToString("dd MMM") + " Value: " + kvp.Value);
-            }
-
-
+            Helpers.PrintToFile();
+            
 
         }
-
-
-        //StreamWriter file = new StreamWriter("log.txt", true);
-        //LogToFileDelegate logToFile = LogToFileDelegate(WriteToFile);
-        //static void WritetoFile(string message)
-        //{
-        //    StreamWriter file = new StreamWriter("log.txt", true);
-        //    file.WriteLine(message);
-        //    file.Close();
-        //}
-
-        //static void AvgTempPerMonth(LogToFileDelegate logToFile)
-        //{
-        //    double avgInside = 5;
-        //    double avgOutside = 5;
-
-        //    //Log to file
-        //    logToFile("avg inside temp per month: " + avgInside);
-        //    logToFile("avg inside temp per month: " + avgOutside);
-        //}
-
-        //static void AvgHumPerMonth(LogToFileDelegate logToFile)
-        //{
-        //    double avgInside = 5;
-        //    double avgOutside = 5;
-
-        //    //Log to file
-        //    logToFile("avg inside hum per month: " + avgInside);
-        //    logToFile("avg inside hum per month: " + avgOutside);
-        //}
-
-        //static void RiskOfMold(LogToFileDelegate logToFile)
-        //{
-        //    double avgInside = 5;
-        //    double avgOutside = 5;
-
-        //    //Log to file
-        //    logToFile("risk of mold inside: " + avgInside);
-        //    logToFile("risk of mold outside: " + avgOutside);
-        //}
-
-        //static void MeteorologicalSeasons(LogToFileDelegate logToFile)
-        //{
-        //    string meteorolocialFallWinter = "";
-        //    logToFile("Meteorological Fall / Winter occurs: " + meteorolocialFallWinter);
-        //}
-
-        //static void MoldCalc(LogToFileDelegate logTofile)
-        //{
-        //    //calc på algoritm
-        //    logTofile("Mold calculation Algorithm: " + "något mer");
-        //}
     }
 }
